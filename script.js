15bc4b8b5841a29a03f7a89c6eb81ea766f2781b
Deb Johnson <friendswithdrj@gmail.com>
2020-12-12 18:01:15 -0600

    Change to the get url for K6

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://localhost:3004/api/more/listings/9000');
+  http.get('http://localhost:3004/api/more/listings/1');
   sleep(1);
 }
